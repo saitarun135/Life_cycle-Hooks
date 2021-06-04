c0a0338cc78e76a92836043bf7d2db1cc8c2abf3
saitarun135 <saitarun135@gm.com>
2021-06-04 09:14:42 +0530

    [feat]:Added the parent component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <Parent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Parent from './components/Parent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+Parent,
   }
 }
 </script>
