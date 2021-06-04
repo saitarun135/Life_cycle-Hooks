9b2793f8cb6edc53d658894c4c1465b608d7d8f6
saitarun135 <saitarun135@gm.com>
2021-06-04 09:33:00 +0530

    [feat]:life cycle methods added in child component

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -2,5 +2,31 @@
 <div>Child-component</div>
 </template>
 <script>
-
+export default{
+    name:'Child',
+    beforeCreate(){
+        console.log('child beforeCreate()')
+    },
+    created(){
+        console.log('child created()')
+    },
+    beforeMount(){
+        console.log('child beforeMount()')
+    },
+    mounted(){
+        console.log('child mounted()')
+    },
+    beforeUpdate(){
+        console.log('child beforeUpdate()')
+    },
+    updated(){
+        console.log('child updated()')
+    },
+    beforeDestroy(){
+        console.log('child before()')
+    },
+    destroyed(){
+        console.log('child destroyed()')
+    }
+}
 </script>
